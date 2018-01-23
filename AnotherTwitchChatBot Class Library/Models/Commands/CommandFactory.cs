f9f586d8f770211318a4ce1004e1c26a7530bedf
Justin Schweigert <jeschwei@go.olemiss.edu>
2018-01-23 17:09:48 -0600

    Added loading of external commands through the "plugins" folder.

--- AnotherTwitchChatBot Class Library/Models/Commands/CommandFactory.cs
+++ AnotherTwitchChatBot Class Library/Models/Commands/CommandFactory.cs
@@ -1,7 +1,9 @@
 ﻿using ATCB.Library.Models.Misc;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -15,11 +17,35 @@ namespace ATCB.Library.Models.Commands
         {
             Commands = new List<Command>();
 
+            // Check to see if plugins folder exists, then try loading
+            if (Directory.Exists($"{AppDomain.CurrentDomain.BaseDirectory}plugins"))
+            {
+                Console.WriteLine("Plugins folder found, attempting to load...");
+                var directoryInfo = new DirectoryInfo($"{AppDomain.CurrentDomain.BaseDirectory}plugins");
+                int pluginCount = 0;
+                foreach (var plugin in directoryInfo.GetFiles("*.dll"))
+                {
+                    var DLL = Assembly.LoadFile(plugin.FullName);
+
+                    foreach (Type type in DLL.GetExportedTypes().Where(x => typeof(Command).IsAssignableFrom(x)))
+                    {
+                        var c = Activator.CreateInstance(type) as Command;
+                        Commands.Add(c);
+                    }
+
+                    pluginCount++;
+                }
+
+                Console.WriteLine($"Added {pluginCount} plugin(s).");
+            }
+
             var commandsEnum = ReflectiveEnumerator.GetEnumerableOfType<Command>();
             foreach (Command c in commandsEnum)
             {
                 Commands.Add(c);
             }
+
+            Console.WriteLine($"Added {Commands.Count} command(s).");
         }
 
         public Command GetCommand(string commandName)
