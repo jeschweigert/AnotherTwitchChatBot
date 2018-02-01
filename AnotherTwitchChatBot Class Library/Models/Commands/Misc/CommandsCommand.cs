119a51e7ea5ee846cc0820337e7381c75b98696f
Justin Schweigert <jeschwei@go.olemiss.edu>
2018-01-31 18:35:30 -0600

    Added Commands command.

---
+++ AnotherTwitchChatBot Class Library/Models/Commands/Misc/CommandsCommand.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ATCB.Library.Models.Commands.Misc
+{
+    public class CommandsCommand : Command
+    {
+        public override bool IsSynonym(string commandText) => commandText == "commands";
+
+        public override void Run(CommandContext context)
+        {
+            context.SendMessage($"@{context.ChatMessage.DisplayName} The list of commands can be found here: https://github.com/sand-head/AnotherTwitchChatBot/wiki/Commands");
+        }
+    }
+}
